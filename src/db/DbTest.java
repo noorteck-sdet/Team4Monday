2e409bb44191a5d1774925f14120c74188310d60
noort <noort@DESKTOP-KKE0VB6>
2024-01-08 19:22:02 -0500

    Initial Commit

---
+++ src/db/DbTest.java
@@ -0,0 +1,12 @@
+package db;
+
+public class DbTest {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void add() {
+		System.out.println( 1 + 2);
+	}
+}
---
+++ src/util/Step.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Step {
+
+	public void display(String str) {
+		System.out.println(str);
+	}
+	
+	public void getData() {
+		System.out.println("Country: USA");
+		System.out.println("State: VA" );
+		System.out.println("City: Reston");
+	}
+}
