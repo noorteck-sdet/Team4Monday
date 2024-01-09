8615d726b3932750ed6e16856bb9a10072857a8b
Habteab <Habteab@habteab>
2024-01-08 21:56:31 -0500

    added a zipcode info....

--- src/util/Step.java
+++ src/util/Step.java
@@ -10,5 +10,7 @@ public class Step {
 		System.out.println("Country: USA");
 		System.out.println("State: VA" );
 		System.out.println("City: Reston");
+		//added zipcod from Mebrat
+		System.out.println("zipcod:1234");
 	}
 }
