b6da66f41ae5817f730b2e20ff44951ef65423ac
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-09 10:56:00 -0500

    correction

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -18,7 +18,7 @@ public class DbTest {
 		System.out.println(10 / 2); // gio
 
 		System.out.println(100 / 5); // Monica
-		System.out.println(50 / 2);
+		System.out.println(50 / 2); // Yasmine
 	}
 
 	public void subtract() {
