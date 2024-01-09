4f633d672f4963a51805bb615da319cb7613df3f
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-08 22:42:30 -0500

    added divide() to main() for running in stage branch

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -5,17 +5,20 @@ public class DbTest {
 	public static void main(String[] args) {
 		DbTest obj = new DbTest();
 		obj.add();
+
+		obj.divide();
 	}
 
 	public void add() {
 		System.out.println(1 + 2);
 	}
-	
+
 	public void divide() {
-		System.out.println( 1 / 2);
+		System.out.println(1 / 2);
 		System.out.println(10 / 2); // gio
-		
+
 		System.out.println(100 / 5); // Monica
-		
+		System.out.println(50 / 2); // Yasmine
+
 	}
 }
