4233d676ec6922fdd342b9d2936c298e6a8d8722
alaza <alaza@LAPTOP-MN9GES92>
2024-01-10 18:59:50 -0500

    push

--- src/util/Step.java
+++ src/util/Step.java
@@ -8,34 +8,34 @@ public class Step {
 	public void display(String str) {
 		System.out.println(str);
 	}
-	
+
 	public void getData() {
 		System.out.println("Country: USA");
-		System.out.println("State: VA" );
+		System.out.println("State: VA");
 		System.out.println("City: Reston");
 
-		//added.. zipcode..
+		// added.. zipcode..
 		System.out.println("Zipcod:1234");
-		
-		//added continent
-		
+
+		// added continent
+
 		System.out.println("Continent: North America");
 		System.out.println("Name");
 	}
-	//ahmed test
-		public static void main(String[] args) {
-			ArrayList<String> Name=new ArrayList<String>();
-			
-			Name.add("ahmed");
-			Name.add("test");
-			ListIterator<String>show=Name.listIterator();
-			
-			while( show.hasNext()) {
-				
-				System.out.println(show.next());
-				
-			}
-	
-		
+
+	// ahmed test to push
+	public static void main(String[] args) {
+		ArrayList<String> Name = new ArrayList<String>();
+
+		Name.add("ahmed");
+		Name.add("test");
+		ListIterator<String> show = Name.listIterator();
+
+		while (show.hasNext()) {
+
+			System.out.println(show.next());
+
+		}
+
 	}
 }
