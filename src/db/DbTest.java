e96cdcf11144bd4eb3360ee49580f8d4b414d10d
alaza <alaza@LAPTOP-MN9GES92>
2024-01-10 19:10:17 -0500

    HashSet

--- src/db/DbTest.java
+++ src/db/DbTest.java
@@ -1,5 +1,7 @@
 package db;
 
+import java.util.LinkedHashSet;
+
 public class DbTest {
 
 	public static void main(String[] args) {
@@ -7,12 +9,21 @@ public class DbTest {
 		obj.add();
 
 		obj.divide();
+         //made by Ahmed 
+		LinkedHashSet<String> teams = new LinkedHashSet<String>();
+
+		teams.add("Team1");
+		teams.add("Team2");
+		teams.add("Team3");
+		teams.add("Team4");
+		System.out.println(teams);
+
 	}
 
 	public void add() {
 		System.out.println(1 + 2);
-		System.out.println(10+5);//Mebrat
-		System.out.println(30+3);//again Mebrat
+		System.out.println(10 + 5);// Mebrat
+		System.out.println(30 + 3);// again Mebrat
 	}
 
 	public void divide() {
@@ -21,20 +32,21 @@ public class DbTest {
 
 		System.out.println(100 / 5); // Monica
 		System.out.println(50 / 2); // Yasmine
-		System.out.println(20/2);// Mebrat
+		System.out.println(20 / 2);// Mebrat
 	}
 
 	public void subtract() {
-		System.out.println(10 - 5); //Yasmine
-		System.out.println(26-1);//Jeremy
-		System.out.println(70-12);//Mebrat
+		System.out.println(10 - 5); // Yasmine
+		System.out.println(26 - 1);// Jeremy
+		System.out.println(70 - 12);// Mebrat
 	}
-	
-	public void multiply (int a, int b) {
-		System.out.println(a*b);// Ivan
+
+	public void multiply(int a, int b) {
+		System.out.println(a * b);// Ivan
 		// Monica commenting from Github
-		
-	System.out.println(56* 8); // Monica
-	System.out.println("Lana");
+
+		System.out.println(56 * 8); // Monica
+		System.out.println("Lana");
+
 	}
 }
